1bae7fc4333344168236519994b50afb7e986f96
Vikas Jaiswal <vikasjaiswal794@gmail.com>
2026-04-03 16:08:52 +0530

    Update Google Maps embed source in MapSection

--- src/components/MapSection.tsx
+++ src/components/MapSection.tsx
@@ -35,7 +35,9 @@ export default function MapSection({ isOpen, onClose }: MapSectionProps) {
             <iframe
               id="mapFrame"
               className="map-frame"
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7538.027498944637!2d72.83559!3d19.07283!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c9c676018b43%3A0x75f29a4205098f99!2sSantacruz%2C%20Mumbai%2C%20Maharashtra!5e0!3m2!1sen!2sin!4v1700000000000"
+              // src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d15000!2d72.843072!3d19.078712!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMTnCsDA0JzQzLjQiTiA3MsKwNTAnNDQuMyJF!5e0!3m2!1sen!2sin!4v1775209698083!5m2!1sen!2sin"
+              src="https://www.google.com/maps?q=Santacruz+East+Mumbai&z=14&output=embed"
+              // src="https://maps.google.com/maps?q=19.078712,72.843072&z=13&output=embed"
               allowFullScreen={false}
               loading="lazy"
               referrerPolicy="no-referrer-when-downgrade"
