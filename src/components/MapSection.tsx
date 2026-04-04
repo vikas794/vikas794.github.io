947b755254e8767e59cdc20b1024dc27b8da1e5d
Vikas Jaiswal <vikasjaiswal794@gmail.com>
2026-04-04 23:55:42 +0530

    Update map iframe source to new Google Maps URL

--- src/components/MapSection.tsx
+++ src/components/MapSection.tsx
@@ -35,9 +35,7 @@ export default function MapSection({ isOpen, onClose }: MapSectionProps) {
             <iframe
               id="mapFrame"
               className="map-frame"
-              // src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d15000!2d72.843072!3d19.078712!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMTnCsDA0JzQzLjQiTiA3MsKwNTAnNDQuMyJF!5e0!3m2!1sen!2sin!4v1775209698083!5m2!1sen!2sin"
               src="https://www.google.com/maps?q=Santacruz+East+Mumbai&z=14&output=embed"
-              // src="https://maps.google.com/maps?q=19.078712,72.843072&z=13&output=embed"
               allowFullScreen={false}
               loading="lazy"
               referrerPolicy="no-referrer-when-downgrade"
